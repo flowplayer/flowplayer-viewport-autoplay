43d2bfacb3e583cff116ea5e8bc7403c41f07055
Christian Ebert <blacktrash@gmx.net>
2017-09-23 12:18:01 +0100

    Compact overlay click code

--- flowplayer.viewport-autoplay.js
+++ flowplayer.viewport-autoplay.js
@@ -30,12 +30,11 @@
         root.appendChild(ap);
 
         ap.addEventListener('click', function(ev) {
+          ev.stopPropagation();
+          ev.preventDefault();
           if (common.hasClass(ev.target, 'pause')) {
-            ev.preventDefault();
             api.toggle();
           } else {
-            ev.stopPropagation();
-            ev.preventDefault();
             api.mute(false);
             common.removeClass(root, 'is-muted-autoplaying');
             if (flowplayer.support.mutedAutoplay) flowplayer.common.find('.fp-engine', root)[0].muted = false;
